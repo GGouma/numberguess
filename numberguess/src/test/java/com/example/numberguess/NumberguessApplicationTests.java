6b363f79a2cc9b41a5eb026fadcaca52f54242b0
GGouma <ggouma34@gmail.com>
2024-03-21 13:47:55 +0900

    초기세팅

---
+++ numberguess/src/test/java/com/example/numberguess/NumberguessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.numberguess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberguessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
