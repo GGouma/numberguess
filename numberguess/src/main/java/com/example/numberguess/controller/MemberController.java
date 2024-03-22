002171e066218381b042584e59e78cf876bc1347
GGouma <ggouma34@gmail.com>
2024-03-22 17:40:09 +0900

    클래스 구성

--- numberguess/src/main/java/com/example/numberguess/controller/MemberController.java
+++ numberguess/src/main/java/com/example/numberguess/controller/MemberController.java
@@ -1,5 +1,12 @@
 package com.example.numberguess.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class MemberController {
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login_form";
+	}
 
 }
